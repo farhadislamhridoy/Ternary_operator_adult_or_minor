3eb2a803e5724894057e573e490112c094709831
Farhad Islam Hridoy <farhadislamhridoy@gmail.com>
2024-09-09 23:07:50 +0600

    Add files via upload

---
+++ Ternary.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+    int age;
+        printf("Enter the age: ");
+        scanf("%d", &age);
+            age>=18? printf("Adult\n"): printf("Minor\n");
+            
+
+    return 0;
+}
